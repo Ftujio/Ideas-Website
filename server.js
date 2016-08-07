97e189a89fd52a7fc614c72c362ce57ed9dfe2f9
Ftujio <svetlin954@abv.bg>
2016-08-07 20:40:49 +0300

    Working on displaying the index page

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+function noPageErr(response){
+	response.writeHead(404, {"Context-Type": "text/plain"});
+	response.write("Error 404: No such page!");
+	response.end();
+}
+
+function onRequest(request, response){
+	if(request.method == 'GET' && request.url == '/'){
+		response.writeHead(200, {"Context-Type": "text/html"});
+		fs.createReadStream("../index.html").pipe(response);
+	} else {
+		noPageErr(response);
+	}
+}
+
+http.createServer(onRequest).listen(8888);	
+console.log("Server is now running....");
