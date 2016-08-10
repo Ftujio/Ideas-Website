092a6077ba0a9dd9d94a60d10b8ded784bf574a2
Ftujio <svetlin954@abv.bg>
2016-08-10 11:05:25 +0300

    Removed unnessecary function

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,11 +6,6 @@ app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/public'));
 
 /* GET home page. */
-router.use(function(req, res, next	){
-	
-	next();
-});
-
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
