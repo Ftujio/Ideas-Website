6fd2ef373a1affe9d6556066ef33ec0502775712
Ftujio <svetlin954@abv.bg>
2016-08-10 14:57:12 +0300

    Made a connection with the database and can get information from it

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/ideaswebsite';
+
+	MongoClient.connect(url, function(err, db){
+		if(err){
+			console.log('Unable to connect to the server ', err);
+		} else {
+			console.log('Connected to server');
+			var collection = db.collection('users');
+
+			collection.find({}).toArray(function(err, result){
+				if(err){
+					console.log(err);
+				} else if(result.length){
+					res.render('index', {"users": result});
+				} else {
+					console.log('No documents found');
+				}
+				db.close();
+			});
+		}
+	});
 });
 
 module.exports = router;
