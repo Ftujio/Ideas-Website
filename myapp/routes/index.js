77360f655c8c93ca46803962c1f6bf61b8e00ec1
Ftujio <svetlin954@abv.bg>
2016-08-11 14:58:59 +0300

    .

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,12 +6,7 @@ db.connect_to_db();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	db.find({"name": "Duudi McGoogly"}, 'users', function(user){
-		console.log(user.name);
-		console.log(user.email);
-		console.log(user.password);
-		console.log(user.account);
-	});
+	
 });
 
 module.exports = router;
