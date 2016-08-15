a38345c74905227dab7f1fef2e8f909be4153ee8
Ftujio <svetlin954@abv.bg>
2016-08-15 13:26:18 +0300

    Configured the packages i just installed

--- myapp/app.js
+++ myapp/app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var session = require('express-session');
-//var mongoose = require('mongoose');
-
-//mongoose.connect('localhost:27017/ideaswebsite');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');	
@@ -16,6 +16,8 @@ var register = require('./routes/register');
 var browse = require('./routes/browse');
 
 var app = express();
+
+mongoose.connect('localhost:27017/ideaswebsite');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +29,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(session({secret: 'secretmessage', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
