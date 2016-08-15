cb1123253bd1a0189bd1fe9b488931ba19b7c25b
Ftujio <svetlin954@abv.bg>
2016-08-15 13:56:47 +0300

    Working on user signup

---
+++ myapp/config/passport.js
@@ -0,0 +1,32 @@
+var passport = require('passport');
+var User = require('../models/user');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.findById(id, function(err, user){
+		done(err, user);
+	});
+});
+
+passport.use('local.signup', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true
+}, function(req, email, password, done){
+	User.findOne({'email': email}, function(err, user){
+		if(err){ // Query error
+			return done(err);
+		}
+		if(user){ // User already exists
+			return done(null, false, {message: 'Email is already in use.'})
+		}
+
+		var newUser = new User();
+		newUser.email = email;
+		newUser.password = password;
+	});
+}));
